4a80b342cb14439b39dce711456156ee11e8d072
wstarywbw12 <wstarywbw@gmail.com>
2026-02-12 10:02:42 +0700

    pertanyaan dan jawaban

--- resources/views/pages/courses/quiz/questions/index.blade.php
+++ resources/views/pages/courses/quiz/questions/index.blade.php
@@ -14,25 +14,31 @@
             <div class="card-body">
                 @foreach ($quiz->questions as $no => $question)
                     <div class="mb-4 border-bottom pb-3">
-                        <strong>{{ $no + 1 }}. {{ $question->question }}</strong>
+                        <strong>{{ $no + 1 }}. {!! $question->question !!}
+                        </strong>
 
-                        <ul class="mt-2">
-                            @foreach ($question->options as $option)
-                                <li>
-                                    {{ $option->option_text }}
-                                    @if ($option->is_correct)
-                                        <span class="badge bg-success">Benar</span>
-                                    @endif
-                                </li>
+                        <div class="mt-2">
+
+                            @foreach ($question->options as $index => $option)
+                                @php
+                                    $label = chr(65 + $index); // A, B, C, D
+                                @endphp
+
+                                <div
+                                    class="d-flex align-items-start mb-1 {{ $option->is_correct ? 'text-success fw-bold' : '' }}">
+                                    <div class="me-2 fw-bold">{{ $label }}.</div>
+                                    <div class="option-text">{!! $option->option_text !!}</div>
+                                </div>
                             @endforeach
-                        </ul>
+
+                        </div>
 
                         <button class="btn btn-sm btn-warning" onclick='openEdit(@json($question))'>
                             <i class="bi bi-pencil"></i> Edit
                         </button>
 
-                        <form id="delete-form-{{ $question->id }}" action="{{ route('quiz.questions.destroy', $question->id) }}"
-                            method="POST" class="d-inline">
+                        <form id="delete-form-{{ $question->id }}"
+                            action="{{ route('quiz.questions.destroy', $question->id) }}" method="POST" class="d-inline">
                             @csrf
                             @method('DELETE')
                             <button type="button" class="btn btn-sm btn-danger"
@@ -58,34 +64,71 @@
     <script>
         const modal = new bootstrap.Modal(document.getElementById('questionModal'));
 
-        function openCreate() {
+        let questionEditor = null;
+        let optionEditors = [];
+
+        async function initEditors() {
+            if (!questionEditor) {
+                questionEditor = await ClassicEditor.create(
+                    document.querySelector('#questionEditor')
+                );
+            }
+
+            const optionEls = document.querySelectorAll('.option-editor');
+            for (let i = 0; i < optionEls.length; i++) {
+                if (!optionEditors[i]) {
+                    optionEditors[i] = await ClassicEditor.create(optionEls[i]);
+                }
+            }
+        }
+
+        function resetEditors() {
+            if (questionEditor) questionEditor.setData('');
+            optionEditors.forEach(editor => editor.setData(''));
+            document.querySelectorAll('.correct-radio').forEach(r => r.checked = false);
+        }
+
+        async function openCreate() {
             const form = document.getElementById('questionForm');
             form.action = "{{ route('quiz.questions.store', $quiz) }}";
             document.getElementById('formMethod').value = 'POST';
             document.getElementById('modalTitle').innerText = 'Tambah Soal';
 
-            document.getElementById('questionText').value = '';
-            document.querySelectorAll('.option-input').forEach(i => i.value = '');
-            document.querySelectorAll('.correct-radio').forEach(r => r.checked = false);
-
             modal.show();
+            await initEditors();
+            resetEditors();
         }
 
-        function openEdit(question) {
+        async function openEdit(question) {
             const form = document.getElementById('questionForm');
-
             form.action = "{{ route('quiz.questions.update', ':id') }}".replace(':id', question.id);
             document.getElementById('formMethod').value = 'PUT';
             document.getElementById('modalTitle').innerText = 'Edit Soal';
 
-            document.getElementById('questionText').value = question.question;
+            modal.show();
+            await initEditors();
+
+            questionEditor.setData(question.question);
 
             question.options.forEach((opt, index) => {
-                document.querySelectorAll('.option-input')[index].value = opt.option_text;
+                optionEditors[index].setData(opt.option_text);
                 document.querySelectorAll('.correct-radio')[index].checked = opt.is_correct;
             });
+        }
 
-            modal.show();
+        function syncEditors() {
+            document.querySelector('#questionEditor').value =
+                questionEditor.getData();
+
+            document.querySelectorAll('.option-editor').forEach((el, index) => {
+                el.value = optionEditors[index].getData();
+            });
         }
     </script>
 @endpush
+
+<style>
+    .option-text p {
+    margin: 0;
+}
+</style>
--- resources/views/pages/courses/quiz/questions/modal.blade.php
+++ resources/views/pages/courses/quiz/questions/modal.blade.php
@@ -1,42 +1,40 @@
 <div class="modal fade" id="questionModal" tabindex="-1">
-  <div class="modal-dialog modal-lg">
-    <form method="POST" id="questionForm">
-        @csrf
-        <input type="hidden" name="_method" id="formMethod" value="POST">
+    <div class="modal-dialog modal-lg">
+        <form method="POST" id="questionForm"  onsubmit="syncEditors()">
+            @csrf
+            <input type="hidden" name="_method" id="formMethod" value="POST">
 
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="modalTitle">Soal Quiz</h5>
-                <button class="btn-close" data-bs-dismiss="modal"></button>
-            </div>
-
-            <div class="modal-body">
-                <div class="mb-3">
-                    <label>Pertanyaan</label>
-                    <textarea name="question" id="questionText"
-                        class="form-control" required></textarea>
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalTitle">Soal Quiz</h5>
+                    <button class="btn-close" data-bs-dismiss="modal"></button>
                 </div>
 
-                @for ($i = 0; $i < 4; $i++)
-                    <div class="input-group mb-2">
-                        <span class="input-group-text">
-                            <input type="radio"
-                                   name="correct_option"
-                                   value="{{ $i }}"
-                                   class="correct-radio">
-                        </span>
-                        <input type="text"
-                               name="options[{{ $i }}][text]"
-                               class="form-control option-input"
-                               placeholder="Pilihan {{ $i+1 }}">
-                    </div>
-                @endfor
-            </div>
+                <div class="modal-body">
 
-            <div class="modal-footer">
-                <button class="btn btn-primary">Simpan</button>
+                    <label class="fw-bold mb-2">Pertanyaan</label>
+                    <textarea name="question" id="questionEditor"></textarea>
+
+                    <hr>
+
+                    @for ($i = 0; $i < 4; $i++)
+                        <div class="mb-3">
+                            <div class="d-flex align-items-center mb-1">
+                                <input type="radio" name="correct_option" value="{{ $i }}"
+                                    class="correct-radio me-2">
+                                <strong>Pilihan {{ $i + 1 }}</strong>
+                            </div>
+
+                            <textarea name="options[{{ $i }}][text]" class="option-editor" data-index="{{ $i }}"></textarea>
+
+                        </div>
+                    @endfor
+                </div>
+
+                <div class="modal-footer">
+                    <button class="btn btn-primary">Simpan</button>
+                </div>
             </div>
-        </div>
-    </form>
-  </div>
+        </form>
+    </div>
 </div>
